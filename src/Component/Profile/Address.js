0f39c9bf770c648b2f5c0abf3ff154476c743586
Youssef-Dhaou <youssefdhaouu@gmail.com>
2022-05-08 15:56:49 +0100

    component-Checkpoint

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+      <div className='add'>
+          <h3 className='discription'> Tunis Gafsa Elguettar Orbata 2180</h3>
+
+      </div>
+
+      )
+}
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+      <div className="add" >
+
+    <h1 className="heading" > Youssef DHAOU</h1>
+  </div>
+  )
+}
+
+export default FullName
